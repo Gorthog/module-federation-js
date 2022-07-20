c5395ad2c412b168d80c4847a80c1b2715c03a70
Amir Shiri <amir@sinapistech.com>
2022-07-20 17:37:04 +0300

    add singleton move index code into bootstrap to make sure we don't fail because of eager loading of shared packages add dev and prod render

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,10 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as productMount } from "products/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
+
+const productRoot = document.querySelector("#my-products");
+productMount(productRoot);
+
+const cartRoot = document.querySelector("#my-cart");
+cartMount(cartRoot);
 
 console.log("container");
